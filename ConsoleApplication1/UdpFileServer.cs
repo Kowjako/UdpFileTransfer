658d01a8acf6424aa7979aaf71b645145f21e69b
Włodzimierz Kowjako <kowyako@yandex.ru>
2020-07-03 11:46:04 +0200

    Add project files.

---
+++ ConsoleApplication1/UdpFileServer.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ConsoleApplication1
+{
+    public class UdpFileServer
+    {
+        [Serializable]
+        public class FileDetails
+        {
+            public string FILETYPE = "";
+            public long FILESIZE = 0;
+        }
+        public static FileDetails fileDet = new FileDetails();
+        static IPEndPoint endPoint;
+        static IPAddress remoteAddress;
+        static int remotePort = 5002;
+        private static UdpClient sender = new UdpClient();
+        private static FileStream fs;
+        [STAThread]
+        static void Main()
+        {
+            try
+            {
+                Console.WriteLine("Write ip of server");
+                remoteAddress = IPAddress.Parse(Console.ReadLine().ToString());
+                endPoint = new IPEndPoint(remoteAddress, remotePort);
+                Console.WriteLine("Write path of file");
+                fs = new FileStream(@Console.ReadLine().ToString(), FileMode.Open, FileAccess.Read);
+                if (fs.Length > 8156)
+                {
+                    Console.WriteLine("File size is unreadable");
+                    sender.Close();
+                    fs.Close();
+                    return;
+                }
+                SendFileInfo();
+                SendFile();
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+            }
+        }
+        static void SendFileInfo()
+        {
+            fileDet.FILETYPE = fs.Name.Substring((int)fs.Name.Length - 3, 3);
+            fileDet.FILESIZE = fs.Length;
+            XmlSerializer xs = new XmlSerializer(typeof(FileDetails));
+            MemoryStream ms = new MemoryStream();
+            xs.Serialize(ms, fileDet);
+            ms.Position = 0;
+            Byte[] data = new Byte[ms.Length];
+            ms.Read(data, 0, Convert.ToInt32(ms.Length));
+            Console.WriteLine("Sending file data is completed");
+            sender.Send(data, data.Length, endPoint);
+            ms.Close();
+        }
+        static void SendFile()
+        {
+            byte[] bytes = new byte[fs.Length];
+            fs.Read(bytes, 0, bytes.Length);
+            Console.WriteLine("Sending file...");
+            try
+            {
+                sender.Send(bytes, bytes.Length, endPoint);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                fs.Close();
+                sender.Close();
+            }
+            Console.WriteLine("File has been sent sucessful");
+            Console.Read();
+        }
+    }
+}
---
+++ ConsoleApplication2/UdpFileClient.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ConsoleApplication2
+{
+    public class UdpFileClient
+    {
+        [Serializable]
+        public class FileDetails
+        {
+            public string FILETYPE = "";
+            public long FILESIZE = 0;
+        }
+        private static FileDetails fileDet;
+        private static int localPort = 5002;
+        private static UdpClient receivingUdpClient = new UdpClient(localPort);
+        private static IPEndPoint RemoteIpEndPoint = null;
+        private static FileStream fs;
+        private static byte[] receiveBytes = new byte[0]; //accepted bytes
+        static void Main()
+        {
+            GetFileDetails();
+            ReceiveFile();
+        }
+        private static void GetFileDetails()
+        {
+            try
+            {
+                Console.WriteLine("Waiting for file details...");
+                receiveBytes = receivingUdpClient.Receive(ref RemoteIpEndPoint);
+                Console.WriteLine("File is sucessfuly read");
+                XmlSerializer xs = new XmlSerializer(typeof(FileDetails));
+                MemoryStream ms = new MemoryStream();
+                ms.Write(receiveBytes, 0, receiveBytes.Length);
+                ms.Position = 0;
+                fileDet = (FileDetails)xs.Deserialize(ms);
+                Console.WriteLine($"Accepted file type: {fileDet.FILETYPE}, file size: {fileDet.FILESIZE}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        private static void ReceiveFile()
+        {
+            try
+            {
+                Console.WriteLine("Waiting for file...");
+                receiveBytes = receivingUdpClient.Receive(ref RemoteIpEndPoint);
+                Console.WriteLine("Saving file on PC...");
+                fs = new FileStream("temp." + fileDet.FILETYPE, FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite);
+                fs.Write(receiveBytes, 0, receiveBytes.Length);
+                Console.WriteLine("File is saved... Opening");
+                Process.Start(fs.Name);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                fs.Close();
+                receivingUdpClient.Close();
+                Console.ReadKey();
+            }
+        }
+    }
+}
